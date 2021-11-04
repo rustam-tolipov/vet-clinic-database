001964f996d820cbcbfe8fce8b41cf5d2eff950a
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-11-04 16:21:39 +0500

    Write queries for animals table
    Written queries for the following:
    Find all animals whose name ends in "mon".
    List the name of all animals born between 2016 and 2019.
    List the name of all animals that are neutered and have less than 3 escape attempts.
    List date of birth of all animals named either "Agumon" or "Pikachu".
    List name and escape attempts of animals that weigh more than 10.5kg
    Find all animals that are neutered.
    Find all animals not named Gabumon.
    Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)

--- queries.sql
+++ queries.sql
@@ -1,3 +1,11 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals WHERE name LIKE '%mon';
+SELECT name FROM animals where date_of_birth BETWEEN DATE '2016-01-01' AND '2019-01-01';
+SELECT name, neutered, escape_attempts FROM  animals WHERE neutered = 'yes' AND escape_attempts < 3;
+SELECT name FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered = 'yes';
+SELECT * FROM animals WHERE name != 'Gabumon';
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
