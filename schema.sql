e076f10d4af5f9fdd14c0808016dea1b28e50c99
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-11-05 14:39:36 +0500

    Insert dates for new tables

--- schema.sql
+++ schema.sql
@@ -37,3 +37,26 @@ ALTER TABLE animals ADD COLUMN owner_id INT,
   ADD CONSTRAINT fk_owners
   FOREIGN KEY (owner_id)
   REFERENCES owners (id);
+
+CREATE TABLE vets(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(65),
+  age INT,
+  date_of_graduation DATE,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE specialization(
+  species_id INT NOT NULL,
+  vets_id INT NOT NULL,
+  CONSTRAINT species_fk FOREIGN KEY (species_id) REFERENCES species(id),
+  CONSTRAINT vets_id FOREIGN KEY (vets_id) REFERENCES vets(id)
+);
+
+CREATE TABLE visits(
+  animals_id INT NOT NULL,
+  vets_id INT NOT NULL,
+  date_of_visit DATE NOT NULL,
+  CONSTRAINT animals_fk2 FOREIGN KEY (animals_id) REFERENCES animals(id),
+  CONSTRAINT vets_fk2 FOREIGN KEY (vets_id) REFERENCES vets(id)
+);
